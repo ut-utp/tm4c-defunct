1d6d31642fb90dc773051b26507eaea52410d4aa
ubuntu <ubuntu@ubuntu1804.com>
2021-06-02 00:17:20 -0400

    some fixes to dma init

--- src/peripherals_tm4c/dma_impl.rs
+++ src/peripherals_tm4c/dma_impl.rs
@@ -23,6 +23,10 @@ static tx_buffer: BBBuffer<U64> = BBBuffer( ConstBBBuffer::new() );
 
 struct DMAStatus(UnsafeCell<u8>);
 
+#[derive(Copy, Clone)]
+#[repr(align(1024))] //control structure must be 1024 byte aligned according to datasheet.
+struct dma_control_structure([u32; 256]);
+
 impl DMAStatus {
     pub fn set_complete(&self, _dma_ind: &int::CriticalSection) {
         // By requiring a CriticalSection be passed in, we know we must
@@ -46,7 +50,11 @@ const DMA_COMPLETE_INDICATOR: DMAStatus = DMAStatus(UnsafeCell::new(0));
 // Should eventually have a dma struct for all peripherals and delegate to peripherals that need to use dma
 pub struct tm4c_uart_dma_ctrl<'a>{
 	power_ctrl: &'a mut tm4c123x::SYSCTL,
-	channel_control: [u32; 256],
+
+	//TODO: abstract this control structure away from here. Better to just have a global variable with some safe features here. 
+	//That way, it's guaranteed to be static lifetime and put in .bss or data segment. Seems strange to initialize a byte aligned
+	//structure on stack anyway.
+	channel_control: dma_control_structure, 
 	device_dma: tm4c123x::UDMA,
 
 	rx_prod: Producer<'a, U64>,
@@ -62,10 +70,11 @@ impl<'a> tm4c_uart_dma_ctrl <'a> {
 
 		let (mut rx_p, rx_c) = rx_buffer.try_split().unwrap();
         let (tx_p, tx_c) = tx_buffer.try_split().unwrap();
+        let control_struct = dma_control_structure([0; 256]);
         
 		Self{
 			power_ctrl: power,
-			channel_control: [0; 256],
+			channel_control: control_struct,
 			device_dma: dma_component,
 			rx_prod: rx_p,
 			rx_cons: rx_c,
@@ -79,7 +88,7 @@ impl<'a> tm4c_uart_dma_ctrl <'a> {
 impl<'a> gen_dma::DmaChannel for tm4c_uart_dma_ctrl <'a>{
 
     fn dma_device_init(&mut self){
- 	    let channel_base_addr = &self.channel_control as *const u32;
+ 	    let channel_base_addr = &self.channel_control.0 as *const u32;
 
  	    let mut rx_grant = self.rx_prod.grant_exact(32).unwrap();
 
@@ -87,15 +96,16 @@ impl<'a> gen_dma::DmaChannel for tm4c_uart_dma_ctrl <'a>{
  	    self.device_dma.cfg.write(|w| unsafe{w.bits(1)});
  	    self.device_dma.ctlbase.write(|w| unsafe{w.bits(channel_base_addr as u32)});
 
- 	    let mut uart_rx_control_slice: &mut [u32] = &mut self.channel_control[tm4c_dma_uart0_rx_control_index..tm4c_dma_uart0_rx_control_index+4];
+ 	    let mut uart_rx_control_slice: &mut [u32] = &mut self.channel_control.0[tm4c_dma_uart0_rx_control_index..tm4c_dma_uart0_rx_control_index+4];
 
- 	     uart_rx_control_slice[0] = unsafe{&((*hal::serial::UART0::ptr()).dr) as *const Reg<u32, hal::tm4c123x::uart0::_DR> as u32}; // Works but is it necessary? Better way to get a raw pointer to uart data register?
+ 	     //uart_rx_control_slice[0] = unsafe{&((*hal::serial::UART0::ptr()).dr) as *const Reg<u32, hal::tm4c123x::uart0::_DR> as u32}; // Works but is it necessary? Better way to get a raw pointer to uart data register?
  	     uart_rx_control_slice[0] = 0x4000_c000 as *const u32 as u32;  // index entry of the control struct is source address (UART data register in this case)
- 	     uart_rx_control_slice[1] = rx_grant.buf().as_ptr() as u32;    // index entry 1 is destination address. Point to bbqueue buffer
+ 	     uart_rx_control_slice[1] = (rx_grant.buf().as_ptr() as u32) + 63;    // index entry 1 is destination address end. Point to bbqueue buffer end
 
 
  	     //index 2 is DMA channel control struct. Check datasheet page 611 for details. Here it represents dest addr increment by 1 byte; src addr fixed; 1 byte each; basic mode. Yet to complete and decide this
- 	     uart_rx_control_slice[2] = 0x00 | (0xA << 14) | (0x1 << 29);
+ 	     //also create some abstractions and maybe a little dma API for tm4c to make this more generic rather than harcoding what we want it to do
+ 	     uart_rx_control_slice[2] = 0x0c00_0000 | 0x0311;
 
     }
 
